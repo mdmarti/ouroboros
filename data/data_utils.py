6fcd9e7ad48e82d8954dc4c9545d5faf4bf4bc40
mdmarti <miles.martinez@duke.edu>
2025-03-06 16:21:25 -0500

    add 2nd deriv interpolation to dataset

--- data/data_utils.py
+++ data/data_utils.py
@@ -2,12 +2,15 @@ from torch.utils.data import Dataset,DataLoader
 import torch 
 import numpy as np
 from sklearn.model_selection import train_test_split
-
+from utils import deriv_approx_d2y,deriv_approx_dy,spline_approx_signal
 
 class aud_neur_ds(Dataset):
 
     def __init__(self,data):
-        self.data = data
+        self.x = data
+        self.dx = deriv_approx_dy(data)
+        self.dx2 = deriv_approx_d2y(data)
+        
 
     def __len__(self):
 
@@ -15,13 +18,20 @@ class aud_neur_ds(Dataset):
 
     def __getitem__(self, idx):
 
-        sample = self.data[idx]
-        x,y = sample[:-1,:],sample[1:,:]
+        x = self.x[idx]
+        dx = self.dx[idx]
+        dx2 = self.dx2[idx]
+        
+        x = torch.from_numpy(x).type(torch.float32)
+        dx = torch.from_numpy(dx).type(torch.float32)
+        dx2 = torch.from_numpy(dx2).type(torch.float32)
 
-        x,y = torch.from_numpy(x).type(torch.DoubleTensor),torch.from_numpy(y).type(torch.DoubleTensor)
-
-        return x,y
+        return x,dx,dx2
     
+    def _spline_interp_d2y(self,dt,lam=5):
+
+        self.dx2 = spline_approx_signal(self.dx2,dt,lam,to_torch=False)
+
 def time_stretch(data,true_dt,fake_dt):
 
     L = len(data)
@@ -44,7 +54,9 @@ def adjusted_euler_integrate(y0,dy,d2y,dt=1):
 
     return y0 + np.cumsum(dy_adjusted,axis=1)
 
-def get_loaders(data,num_workers=4,batch_size=32,train_size=0.8,cv = False,seed=None):
+def get_loaders(data,dt=1/44100,num_workers=4,batch_size=32,\
+                train_size=0.8,cv = False,seed=None,\
+                interp_d2y=False):
 
     dls = {}
     
@@ -56,8 +68,18 @@ def get_loaders(data,num_workers=4,batch_size=32,train_size=0.8,cv = False,seed=
     if cv:
         X_val, X_test = train_test_split(X_test,test_size=0.5,random_state=seed)
         dsVal = aud_neur_ds(X_val)
+        if interp_d2y:
+            print('interpolating validation 2nd derivs...')
+            dsVal._spline_interp_d2y(dt)
+            print('done!!')
         dls['val'] = DataLoader(dsVal,num_workers=num_workers,batch_size=batch_size,shuffle=False)
     dsTrain,dsTest = aud_neur_ds(X_train),aud_neur_ds(X_test)
+    if interp_d2y:
+        print('interpolating second derivs for train set...')
+        dsTrain._spline_interp_d2y(dt)
+        print('interpolating second derivs for test set....')
+        dsTest._spline_interp_d2y(dt)
+        print('done!')
     dls['train'] = DataLoader(dsTrain,num_workers=num_workers,batch_size=batch_size,shuffle=True)
     dls['test'] = DataLoader(dsTest,num_workers=num_workers,batch_size=batch_size,shuffle=False)
 
