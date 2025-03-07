8cc9408653bc34478e2a2ec2f8ab57783107da25
mdmarti <miles.martinez@duke.edu>
2025-03-07 15:42:13 -0500

    try, fail to fix eval errors

--- train/eval.py
+++ train/eval.py
@@ -21,16 +21,13 @@ def eval_model_error(dls,model,dt,comparison='val'):
 
     for idx,batch in enumerate(dls['train']):
         with torch.no_grad():
-            x,y = batch # each is bsz x seq len x n neurons + 1
+            x,dy,dy2 = batch # each is bsz x seq len x n neurons + 1
             bsz,_,n = x.shape
 
             x = x.to('cuda').to(torch.float32)
-            y = y.to('cuda').to(torch.float32)
+            dy = dy.to('cuda').to(torch.float32)
+            dy2 = dy2.to('cuda').to(torch.float32)/(dt**2)
 
-            dy = deriv_approx_dy(x)
-            # dy_4dt, dy_3dt, ...., dy_(L-4)dt
-            #change: scaling to "true" d2y
-            dy2 = deriv_approx_d2y(x)/(dt**2)
             y2hat,state_pred,trend_penalty = model(x,dt,use_trend_filtering=model.trend_filtering) #state: B x L x SD
             
             # change: scaling to "true" d2y
@@ -54,15 +51,11 @@ def eval_model_error(dls,model,dt,comparison='val'):
 
     for idx,batch in enumerate(dls[comparison]):
         with torch.no_grad():
-            x,y = batch
+            x,dy,dy2 = batch
             x = x.to('cuda').to(torch.float32)
-            y = y.to('cuda').to(torch.float32)
-            
-            dy = deriv_approx_dy(x)
-            # dy_4dt, dy_3dt, ...., dy_(L-4)dt
-            #scaling to "true" d2y
-            dy2 = deriv_approx_d2y(x)/(dt**2)
-            # d2y_4dt, d2y_5dt, ..., d2y_(L-4)dt            
+            dy = dy.to('cuda').to(torch.float32)
+            dy2 = dy2.to('cuda').to(torch.float32)/(dt**2)
+       
             
             y2hat,state_pred,penalty = model(x,dt,use_trend_filtering=model.trend_filtering) #state: B x L x SD
     
@@ -122,18 +115,18 @@ def eval_model_integration(dls,model,dt,n_segs=100,st=0.05,comparison='val'):
         train_b = dls['train'].dataset[train_ind]
         test_b = dls[comparison].dataset[test_ind]
 
-        train_x,train_y = train_b
-        test_x,test_y = test_b 
+        train_x,train_dy,train_dy2 = train_b
+        test_x,test_dy,test_dy2 = test_b 
         train_x,test_x = train_x.view(1,len(train_x),1),test_x.view(1,len(test_x),1)
         
         test_y_modelint,*_ = model.integrate(test_x.to('cuda').to(torch.float32),dt,with_residual=False,method='RK45',st=0.0)
         train_y_modelint,*_ = model.integrate(train_x.to('cuda').to(torch.float32),dt,with_residual=False,method='RK45',st=0.0)
-        test_dy2,train_dy2 = deriv_approx_d2y(test_x)/(dt**2), deriv_approx_d2y(train_x)/(dt**2)
+        test_dy2,train_dy2 = test_dy2/(dt**2),train_dy2/(dt**2) 
         test_dy2,train_dy2 = test_dy2.detach().cpu().numpy().squeeze(),train_dy2.detach().cpu().numpy().squeeze()
         test_dy,train_dy = deriv_approx_dy(test_x)/(dt), deriv_approx_dy(train_x)/(dt)
         test_dy,train_dy = test_dy.detach().cpu().numpy().squeeze(),train_dy.detach().cpu().numpy().squeeze()
         train_x,test_x = train_x.detach().cpu().numpy().squeeze()[4:-4],test_x.detach().cpu().numpy().squeeze()[4:-4]
-        train_y, test_y = train_y.detach().cpu().numpy().squeeze()[3:-5],test_y.detach().cpu().numpy().squeeze()[3:-5]
+        #train_y, test_y = train_y.detach().cpu().numpy().squeeze()[3:-5],test_y.detach().cpu().numpy().squeeze()[3:-5]
         L = len(train_x)
         L_train,L_test = min(L,train_y_modelint.shape[1]),min(L,test_y_modelint.shape[1])
         
@@ -143,11 +136,11 @@ def eval_model_integration(dls,model,dt,n_segs=100,st=0.05,comparison='val'):
         #print(test_dy.shape)
         #print(z0_test.shape)
 
-        test_yint = remove_rm(integrate_d2y(test_dy2,t_samples=t,init_cond=z0_test),rm_length=5)[0,start:L_test]
-        train_yint = remove_rm(integrate_d2y(train_dy2,t_samples=t,init_cond=z0_train),rm_length=5)[0,start:L_train]
+        #test_yint = remove_rm(integrate_d2y(test_dy2,t_samples=t,init_cond=z0_test),rm_length=5)[0,start:L_test]
+        #train_yint = remove_rm(integrate_d2y(train_dy2,t_samples=t,init_cond=z0_train),rm_length=5)[0,start:L_train]
         train_y_modelint = remove_rm(train_y_modelint.squeeze(),rm_length=5)[0,start:L_train]
         test_y_modelint = remove_rm(test_y_modelint.squeeze(),rm_length=5)[0,start:L_test]
-        train_y,test_y = train_y[start:L_train],test_y[start:L_test]
+        train_y,test_y = train_x[start+1:L_train],test_x[start+1:L_test]
         
         train_errs.append(np.abs(train_y_modelint - train_y))
         test_errs.append(np.abs(test_y_modelint - test_y))
